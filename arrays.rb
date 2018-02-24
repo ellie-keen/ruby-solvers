2f8c650eeb308033a88077f16703b874a90cad2b
Jessica Barclay <jessicabarclay.net@gmail.com>
2018-02-24 23:02:25 +0000

    Add combine_arrays method, to be completed

--- arrays.rb
+++ arrays.rb
@@ -4,3 +4,15 @@
 def double_numbers(array)
 	array.map { |integer| integer * 2 }	
 end
+
+# This method which takes two arrays as arguments,
+# should return a new array as follows
+
+# a = [1, 2, 3]
+# b = [4, 5, 6]
+
+# combine_arrays(a, b)
+# returns --> [1, 4, 2, 5, 3, 6]
+
+def combine_arrays(array_1, array_2)
+end
