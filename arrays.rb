b000b30e9869cdfe7c60d0f54349185ed7242ef0
Jessica Barclay <jessicabarclay.net@gmail.com>
2018-02-24 23:19:08 +0000

    Add highest_total_value method, to be completed

--- arrays.rb
+++ arrays.rb
@@ -7,7 +7,7 @@ end
 
 # This method which takes two arrays as arguments,
 # should return a new array as follows
-
+# i.e.
 # a = [1, 2, 3]
 # b = [4, 5, 6]
 
@@ -15,4 +15,17 @@ end
 # returns --> [1, 4, 2, 5, 3, 6]
 
 def combine_arrays(array_1, array_2)
+end
+
+# Update this method, it takes two arrays arguments
+# It should return the total from the array with
+# the highest total value
+# i.e.
+# a = [1, 2, 3]
+# b = [4, 5, 6]
+
+# combine_arrays(a, b)
+# returns --> 15
+
+def highest_total_value(array_1, array_2)
 end
