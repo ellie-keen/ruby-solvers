bbcac631b886e87e1225f4c5719a4727a8de7a38
Jessica Barclay <jessicabarclay.net@gmail.com>
2018-02-24 17:14:31 +0000

    Added strings.rb, contains the method names

---
+++ strings.rb
@@ -0,0 +1,10 @@
+# Using string interpolation
+# Create a method, which takes two names as strings
+# and returns the names as follows
+
+# KEEN, Ellie
+
+
+def names(first_name, last_name)
+	"#{last_name.upcase}, #{first_name.capitalize}"
+end
