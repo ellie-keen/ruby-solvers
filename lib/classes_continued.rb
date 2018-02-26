cc8bc706674582a607b6f194e79329d0d0789b8b
Jessica Barclay <jessicabarclay.net@gmail.com>
2018-02-26 22:16:45 +0000

    Create classes_continued.rb problem to be solved; Includes failing tests

---
+++ lib/classes_continued.rb
@@ -0,0 +1,10 @@
+# Feel free to use your class from class.#!/usr/bin/env ruby -wKU
+
+# This class should:
+#  - Have a read & write property called age
+#  - age should be set to 0 by default
+#  - Have a method called another_year_older
+#	 - another_year_older should increase age by 1 when called
+
+class Person
+end
