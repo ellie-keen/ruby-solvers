f269dbd68f24cf79aa96c3f0922480f44f167d4b
Ellie Keen <elliekeen95@gmail.com>
2018-03-05 13:13:06 +0000

    Name and age reader/write method  complete for classes.rb

--- lib/classes.rb
+++ lib/classes.rb
@@ -3,4 +3,22 @@
 # You should be able to read and write to both Name & Age
 
 class Person
+
+	def name=(name)
+		@name = name
+	end
+
+	def age=(age)
+		@age = age
+	end
+
+	def name
+		@name
+	end
+
+	def age
+		@age
+	end
+
 end
+
