a79fbdf6b6b4f906593ad638682571b527eb1846
Jessica Barclay <jessicabarclay.net@gmail.com>
2018-03-07 21:05:11 +0000

    clean up tests

--- spec/classes_continued_spec.rb
+++ spec/classes_continued_spec.rb
@@ -1,19 +1,18 @@
 require_relative '../lib/classes_continued.rb'
 
 describe Person do
+  alias_method :person, :subject
   it { is_expected.to respond_to(:age=) }
   it { is_expected.to respond_to(:age) }
   
   describe '#age' do
   	it 'should be set to 0 by default' do
-			person = Person.new
   		expect(person.age).to eq 0
  	  end
   end
   
   describe '#another_year_older' do
   	it 'should increase Person age by 1' do
-  		person = Person.new
   		expect { person.another_year_older }.to change { person.age }.by(1)
   	end
   end
