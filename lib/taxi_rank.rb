282a4f825e661579ecc3e721e8c98b9567f2ceaa
Jessica Barclay <jessicabarclay.net@gmail.com>
2018-03-07 21:22:20 +0000

    update instructions in taxi_rank.rb

--- lib/taxi_rank.rb
+++ lib/taxi_rank.rb
@@ -4,6 +4,7 @@ require_relative './taxi.rb'# this line allows you to use your Taxi class
 
 #  - have a read attribute called car_park
 #  - car_park should be an empty array by default
+#    set this in the INITIALIZE method
 #  - have a method called park, which takes a Taxi as an argument,
 #    and adds it to the car_park array
 #  - have a method called release_taxi, which will remove the last
