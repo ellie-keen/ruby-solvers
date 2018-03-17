75682e0c5875a057f400d1733d6e823495489174
Ellie Keen <elliekeen95@gmail.com>
2018-03-17 12:02:03 +0000

    Playing card exercise complete

--- lib/playing_card.rb
+++ lib/playing_card.rb
@@ -4,3 +4,18 @@ Create a class called PlayingCard
 it should have a read write attribute called value
 	
 =end
+
+class PlayingCard
+	def value
+		@value
+	end
+
+	def value=(value)
+		@value = value
+	end
+end
+
+card = PlayingCard.new
+
+puts card.value=('king of hearts')
+puts card.value
