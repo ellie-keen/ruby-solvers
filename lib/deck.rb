358dc940e05b29dd2d653bdfee3836b9439a0965
Ellie Keen <elliekeen95@gmail.com>
2018-03-17 12:56:51 +0000

    Deck exercise complete

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,13 +1,39 @@
+require_relative 'playing_card'
+
 =begin
 	
 Create a class called Deck
-	it should have a read write attribute 
-	called card which should be an array(empty by default)
+	it should have a read attribute 
+	called cards which should be an array(empty by default)
 
 	it should have a method called add which takes a card and adds it to cards
 
-	it should have a method called shuffle_and_draw
-	which returns a random card from the deck cards array
+	it should have a method called shuffle
+	which shuffles the cards array
 
+	it should have a method called draw which draws a 
+	random card from the cards array
 	
 =end
+
+class Deck
+	def initialize
+		@cards = []
+	end
+
+	def cards
+		@cards
+	end
+
+	def add(card)
+		@cards << card
+	end
+
+	def shuffle
+		@cards.shuffle!
+	end
+
+	def draw
+		@cards.sample
+	end
+end
